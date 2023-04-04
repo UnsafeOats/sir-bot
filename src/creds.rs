997f22f597708de39f7b610a21886a3964882f03
Shane Stephenson <stephenson.shane.a@gmail.com>
2023-04-03 20:06:00 -0400

    first working version of the bot that interacts with ChatGPT

--- src/creds.rs
+++ src/creds.rs
@@ -1,5 +1,17 @@
-// TODO: create a credentials struct that can load the following info from a .bot.env toml file in the root
-// directory:
-// - bot application id (key: CRABGPT_DISCORD_APP_ID)
-// - bot bearer token (key: CRABGPT_DISCORD_TOKEN)
-// - openai token (key: OPENAI_API_KEY)
+use serde::Deserialize;
+use std::fs;
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct CrabCredentials {
+    pub discord_app_id: u64,
+    pub discord_token: String,
+    pub discord_name: String,
+    pub openai_api_key: String,
+}
+
+impl CrabCredentials {
+    pub fn new() -> Self {
+        let file = fs::read_to_string(".bot.env").expect("[error] Unable to read .bot.env file");
+        toml::from_str(&file).expect("[error] Unable to parse .bot.env file")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,52 @@
+// To use the below bot, you need a file named .bot.env in the root of the project with the
+// following fields:
+// openai_api_key="your openai api key"
+// discord_app_id=your discord app id
+// discord_token="your discord bot token"
+use chatgpt::prelude::*;
+use crab_gpt_bot::creds::CrabCredentials;
 use serenity::{
     async_trait,
+    framework::standard::StandardFramework,
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
-use csv::Writer;
-use std::error::Error;
-use std::path::Path;
-use crate::openai_client::fetch_openai_completion;
 
-struct Handler;
+struct Handler {
+    bot_client: ChatGPT,
+}
+
+impl Handler {
+    pub fn new(bot_creds: CrabCredentials) -> Self {
+        let bot_client = ChatGPT::new(bot_creds.openai_api_key)
+            .expect("[error] Unable to create ChatGPT client");
+        Self { bot_client }
+    }
+}
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content.starts_with("%crabby") {
-            let prompt = msg.content.trim_start_matches("%crabby").trim();
-            match fetch_openai_completion(prompt).await {
-                Ok(completion) => {
-                    if let Err(why) = msg.channel_id.say(&ctx.http, &completion).await {
+        if msg.content.to_lowercase().starts_with("yo crabby") {
+            let prompt = msg
+                .content
+                .to_lowercase()
+                .trim_start_matches("yo crabby")
+                .trim()
+                .to_string();
+            match self.bot_client.send_message(prompt).await {
+                Ok(response) => {
+                    if let Err(why) = msg
+                        .channel_id
+                        .say(&ctx.http, &response.message().content)
+                        .await
+                    {
                         println!("Error sending message: {:?}", why);
                     }
                 }
                 Err(why) => {
                     println!("Error fetching OpenAI completion: {:?}", why);
                 }
-            }
-        } else if msg.content.starts_with("%collect") {
-            if let Err(why) = collect_messages(&ctx, &msg).await {
-                println!("Error collecting messages: {:?}", why);
-            }
-        } else if msg.content.to_lowercase().contains("hello") {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Yarrr me matey, howdy!").await {
-                println!("Error sending message: {:?}", why);
             }
         }
     }
@@ -41,35 +56,16 @@ impl EventHandler for Handler {
     }
 }
 
-async fn collect_messages(ctx: &Context, msg: &Message) -> Result<(), Box<dyn Error + Send + Sync>> {
-    let mut writer = Writer::from_path(Path::new("collected_messages.csv"))?;
-
-    let channels = msg.guild_id.unwrap().channels(&ctx.http).await?;
-    for (_channel_id, channel) in channels {
-        if let Some(messages) = channel.messages(&ctx.http, |retriever| retriever.limit(100)).await.ok() {
-            for message in messages {
-                writer.write_record(&[message.author.name, message.content])?;
-            }
-        }
-    }
-
-    writer.flush()?;
-    msg.channel_id.say(&ctx.http, "Messages have been collected!").await?;
-    Ok(())
-}
-
 #[tokio::main]
 async fn main() {
-    let token = std::env::var("CRABGPT_DISCORD_TOKEN").expect("Expected a token in the environment");
-
-    let application_id: u64 = std::env::var("CRABGPT_APPLICATION_ID")
-        .expect("Expected an application ID in the environment")
-        .parse()
-        .expect("Application ID is not valid");
-
-    let mut client = Client::builder(token)
-        .application_id(application_id)
-        .event_handler(Handler)
+    let creds = CrabCredentials::new();
+    let handler = Handler::new(creds.clone());
+    let framework = StandardFramework::new().configure(|c| c.prefix("yo crabby"));
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(creds.discord_token, intents)
+        .application_id(creds.discord_app_id)
+        .event_handler(handler)
+        .framework(framework)
         .await
         .expect("Error creating client");
 
@@ -77,4 +73,3 @@ async fn main() {
         println!("Client error: {:?}", why);
     }
 }
-
